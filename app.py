5d64ecfdac9340d33051c52f2a984964eb6e42a1
Kevin Bui <khvbui@gmail.com>
2022-06-24 12:58:48 -0700

    Complete the Web App
    -Created the form so the user can input values: room and distance
    -Created the predict function which can read the inputted values and output the estimated price

--- app.py
+++ app.py
@@ -8,5 +8,19 @@ model = pickle.load(open('model.pkl', 'rb')) # load the model
 def home():
     return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    """Grabs the input values and uses them to make predictions"""
+    rooms = int(request.form["rooms"])
+    distance = int(request.form["distance"])
+    
+    prediction = model.predict([[rooms, distance]]) # returns a list
+    output = round(prediction[0], 2) # pick only first element from list
+
+    return render_template('index.html',
+            prediction_text = f'A house with {rooms} rooms and located '
+                              f'{distance} km from the city center has a '
+                              f'value of ${output}K')
+
 if __name__ == "__main__":
     app.run()
